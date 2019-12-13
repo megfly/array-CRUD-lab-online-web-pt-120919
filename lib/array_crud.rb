dadf208c4b5d792467ef307b60da808d8bffb360
Meghan Flynn <mflynn333@gmail.com>
2019-12-13 20:44:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [tacos, cake, ice cream, salad]
+  ["tacos", "salad", "cake", "ice cream"]
 end
 
 def add_element_to_end_of_array(array, element)
